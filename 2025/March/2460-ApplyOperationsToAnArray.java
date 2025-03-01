3a82c195f496ffc914a0ea605d0da5259d179c9f
Akshitha Naverse <142154380+Akshithanaverse@users.noreply.github.com>
2025-03-01 21:24:30 +0530

    Update 2460-ApplyOperationsToAnArray.java

--- 2025/March/2460-ApplyOperationsToAnArray.java
+++ 2025/March/2460-ApplyOperationsToAnArray.java
@@ -8,14 +8,36 @@ class Solution {
                 nums[i+1]=0;
             }
         }
-        int[] res=new int[n];
-        int i=0;
-        for(int j=0;j<n;j++)
-        {
-            if(nums[j]!=0) {
-                res[i++]=nums[j];
+        // int[] res=new int[n];
+        // int i=0;
+        // for(int j=0;j<n;j++)
+        // {
+        //     if(nums[j]!=0) {
+        //         res[i++]=nums[j];
+        //     }
+        // }
+        //return res;
+        
+        //Optimal approach for moving zeroes to the end of the array
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0)
+            {
+                j=i;
+                break;
             }
         }
-        return res;
+        if(j==-1) return nums;
+        for(int i=j+1;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        return nums;
     }
 }
