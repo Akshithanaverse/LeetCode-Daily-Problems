36c1b3619e1328c8b8aa942af420ffddc2a87e41
Akshitha Naverse <142154380+Akshithanaverse@users.noreply.github.com>
2025-03-01 20:15:59 +0530

    Update 2460-ApplyOperationsToAnArray.java

--- 2025/March/2460-ApplyOperationsToAnArray.java
+++ 2025/March/2460-ApplyOperationsToAnArray.java
@@ -1,1 +1,21 @@
 //https://leetcode.com/problems/apply-operations-to-an-array/description/?envType=daily-question&envId=2025-03-01
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++) {
+            if(nums[i]==nums[i+1]) {
+                nums[i]*=2;
+                nums[i+1]=0;
+            }
+        }
+        int[] res=new int[n];
+        int i=0;
+        for(int j=0;j<n;j++)
+        {
+            if(nums[j]!=0) {
+                res[i++]=nums[j];
+            }
+        }
+        return res;
+    }
+}
